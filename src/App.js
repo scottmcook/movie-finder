d43d150b88557dfa115b2340963f5642af0131ab
Cook <scott.madison.cook@gmail.com>
2020-05-15 14:24:17 -0700

    Converted to hooks

--- src/App.js
+++ src/App.js
@@ -1,60 +1,98 @@
-import React, { Component } from "react";
-import FilmCard from "./FilmCard";
+import React, { useReducer, useEffect } from "react";
+import FilmCard from "./components/FilmCard";
+import FilmSearch from "./components/FilmSearch";
 
 const api_key = `${process.env.REACT_APP_TMDB_API_KEY}`;
-const api = `https://api.themoviedb.org/3/movie/76341?api_key=${api_key}`;
+const TMDB_API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=spider%20man&page=1&include_adult=false`;
+// const TMDB_API_URL = `https://www.omdbapi.com/?s=man&apikey=4a3b711b`;
 
 // https://developers.themoviedb.org/3/getting-started/images
-const base_img_url = `https://image.tmdb.org/t/p`;
 
-class App extends Component {
-  state = {
-    isLoading: true,
-    film: {},
-    error: null,
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null,
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload,
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error,
+      };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(TMDB_API_URL)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: jsonResponse.Search,
+        });
+      });
+  }, []);
+
+  const search = (searchValue) => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST",
+    });
+
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${searchValue}&page=1&include_adult=false`
+      // `https://www.omdbapi.com/?${searchValue}&apikey=4a3b711b`
+    )
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        if (jsonResponse.Response === "True") {
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.Search,
+          });
+        } else {
+          dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.Error,
+          });
+        }
+      });
   };
 
-  fetchUsers() {
-    // Where we're fetching data from
-    fetch(api)
-      // We get the API response and receive data in JSON format...
-      .then((response) => response.json())
-    
-      // ...then we update the film state
-      .then((data) =>
-        this.setState({
-          film: data,
-          isLoading: false, 
-        })
-        
-      )
-      // Catch any errors we hit and update the app
-      .catch((error) => this.setState({ error, isLoading: false }));
-  }
-
-  componentDidMount() {
-    this.fetchUsers();
-  }
-
-  render() {
-    const { isLoading, film, error } = this.state;
-    const { id, homepage, poster_path, title } = film;
-
-    return (
-      <>
-        {error ? <p>{error.message}</p> : null}
-        {!isLoading ? (
-          <FilmCard
-            title={title}
-            poster={poster_path}
-          />
-        ) : (
-          // If there is a delay in data, let's let the user know it's loading
-          <h3>Loading...</h3>
-        )}
-      </>
-    );
-  }
-}
+  const { movies, errorMessage, loading } = state;
+  return (
+    <>
+      <FilmSearch search={search} />
+      {loading && !errorMessage ? (
+        <span>loading...</span>
+      ) : errorMessage ? (
+        <div className="errorMessage">{errorMessage}</div>
+      ) : (
+        movies.map((movie, index) => (
+          <FilmCard key={`${index}-${movie.Title}`} movie={movie} />
+        ))
+      )}
+    </>
+  );
+};
 
 export default App;
