03ea12b332291374ce104b39967a461c37696a60
Cook <scott.madison.cook@gmail.com>
2020-05-15 14:25:05 -0700

    New search component

---
+++ src/components/FilmSearch.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+
+
+const FilmSearch = (props) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchInputChanges = (e)  => {
+    setSearchValue(e.target.value);
+  }
+
+  const resetInputField = () => {
+    setSearchValue("")
+  }
+
+  const callSearchFunction = (e) => {
+    e.preventDefault();
+    props.search(searchValue);
+    resetInputField();
+  }
+
+
+  return (
+    <form>
+      <TextField 
+        label="Search Film"
+        value={searchValue}
+        onChange={handleSearchInputChanges}
+        variant="outlined" 
+        />
+        <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+    </form>
+  );
+};
+
+export default FilmSearch;
