10beb62a93ce2988b3b84c74c58bb571e06290c9
Cook <scott.madison.cook@gmail.com>
2020-05-18 20:22:44 -0700

    Refactor

--- src/components/FilmSearch.js
+++ src/components/FilmSearch.js
@@ -3,9 +3,6 @@ import TextField from "@material-ui/core/TextField";
 import Button from '@material-ui/core/Button';
 import Grid from "@material-ui/core/Grid";
 
-
-
-
 const FilmSearch = (props) => {
   const [searchValue, setSearchValue] = useState("");
 
@@ -32,10 +29,17 @@ const FilmSearch = (props) => {
           value={searchValue}
           onChange={handleSearchInputChanges}
           variant="outlined" 
+          required={true}
         />
       </Grid>
       <Grid item>
-        <Button variant="contained" color="primary" onClick={callSearchFunction} type="submit" value="SEARCH">Search</Button>
+        <Button 
+          variant="contained" 
+          color="primary" 
+          onClick={callSearchFunction} 
+          type="submit" 
+          value="SEARCH">Search
+        </Button>
       </Grid>
     </Grid>
   );
