caddcd4ff5dde99d435c08f1fb91dfb29341f7a6
Cook <scott.madison.cook@gmail.com>
2020-05-18 20:22:35 -0700

    Removed unused code, refactored, added conditional

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -17,17 +15,16 @@ const useStyles = makeStyles({
   },
 });
 
-//let searchApi = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${searchQuery}&page=1&include_adult=false`
-
-// https://developers.themoviedb.org/3/getting-started/images
 const DEFAULT_POSTER = `https://via.placeholder.com/300x400?text=Poster+Not+Available`;
 const BASE_POSTER_PATH = `https://image.tmdb.org/t/p/w1280/`
+const SPECIAL_POSTER_PATH = 'https://m.media-amazon.com/images/M/MV5BMjM1MTkzNTAxM15BMl5BanBnXkFtZTgwMDQzMDMwNTE@._V1_SY1000_SX675_AL_.jpg'
+
 
 export const FilmCard = ( { movie }) => {
   const classes = useStyles();
-  console.log(movie.poster_path);
   
-  const poster = movie.poster_path === null ? DEFAULT_POSTER : `${BASE_POSTER_PATH}${movie.poster_path}`;
+  const poster = movie.title === 'Sunset Rock' ? SPECIAL_POSTER_PATH 
+                 : movie.poster_path === null ? DEFAULT_POSTER : `${BASE_POSTER_PATH}${movie.poster_path}`;
 
   return (
     <Card className={classes.root} >
@@ -43,15 +40,9 @@ export const FilmCard = ( { movie }) => {
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             {movie.overview}
-            
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button color="primary">
-          Add to Favorites
-        </Button>
-      </CardActions>
     </Card>
   );
 }
