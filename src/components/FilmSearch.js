5a3fc33ff5248d305102dc8a3681d38fd0c3f05f
Cook <scott.madison.cook@gmail.com>
2020-08-06 11:44:41 -0700

    Added page title

--- src/components/FilmSearch.js
+++ src/components/FilmSearch.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from '@material-ui/core/Button';
 import Grid from "@material-ui/core/Grid";
+import Typography from '@material-ui/core/Typography';
 
 const FilmSearch = (props) => {
   const [searchValue, setSearchValue] = useState("");
@@ -28,24 +29,31 @@ const FilmSearch = (props) => {
   }
 
   const handleBlankEntry = () => {
-    alert('Don\'t f\'n do that');
+  
+    return searchValue === "" ? true : false
   }
 
   return (
-    <Grid justify={'center'} alignItems={'center'} container spacing={3}>
-      <Grid item lg={6} xs={8}>
+    <>
+    <Grid container justify={'center'} style={{ margin: 15 }}>
+      <Typography variant="h4">Movie Finder</Typography>
+    </Grid>
+    <Grid container justify={'center'} alignItems={'center'} spacing={3}>
+      <Grid item lg={3} m={6} xs={8}>
         <TextField 
+          autoFocus={true}
+          error={ searchValue === "" ? true : false }
           fullWidth={true}
-          label="Search film database"
+          label="Search TMDB"
           value={searchValue}
           onChange={handleSearchInputChanges}
-          onKeyPress={ searchValue !== "" ? handleEnter : handleBlankEntry }
-          placeholder="Ex: Sunset Rock"
+          onKeyPress={ handleEnter }
+          placeholder="Sunset Rock"
           variant="outlined" 
           required={true}
         />
       </Grid>
-      <Grid item lg={2} xs={8}>
+      <Grid item lg={1} m={6} xs={4}>
         <Button 
           fullWidth={true}
           variant="contained" 
@@ -56,6 +64,7 @@ const FilmSearch = (props) => {
         </Button>
       </Grid>
     </Grid>
+    </>
   );
 };
 
